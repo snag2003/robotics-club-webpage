c337535390fed1334d7bf0d3a891310429735e79
Stephany Acosta <stephanynag@gmail.com>
2024-03-08 13:49:40 -0500

    initial commit setup

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">QLU Robotics Club</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+    </div>
+  );
+}
