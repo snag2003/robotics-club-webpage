21113eb18ef0bfec9c21416df8d76961266a016d
Victor <vicnicferrara@gmail.com>
2024-04-11 10:30:51 -0500

    Sub-Page Organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MainTitle } from "./components/MainTitle/MainTitle";
 import { Box } from './components/Box/Box';
-
 import { Footer } from "./components/Footer/Footer";
 import styles from "../styles/Home.module.css";
 
